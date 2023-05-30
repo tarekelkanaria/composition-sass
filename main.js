6e0f1bab48c5d81c0d9518c59a267b0a74a03229
Tarek Elkanaria <tarekelkanaria@gmail.com>
2023-05-31 00:55:46 +0300

    Add toggle functionality

---
+++ main.js
@@ -0,0 +1,7 @@
+const burgerBTN = document.querySelector(".burger-btn");
+const navBar = document.querySelector(".head__nav");
+burgerBTN.addEventListener("click", () => {
+  navBar.classList.contains("show")
+    ? navBar.classList.remove("show")
+    : navBar.classList.add("show");
+});
